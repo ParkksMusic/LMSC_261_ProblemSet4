a83355848cab48f28bb34cc0c54bd175446fb03a
Benjamin Parkka <Bparkka@comcast.net>
2019-10-15 21:10:13 -0400

    ProblemSet4

--- ProblemSet4_1.c
+++ ProblemSet4_1.c
@@ -1,1 +1,60 @@
+#include <stdio.h>
 
+char pixels[200];
+char compressed[200];
+
+int read(void);
+int write(void);
+void encode(void);
+
+int main(){
+    if(read() == 1) return 1;
+    encode();
+    if(write() == 1) return 1;
+
+    return 0;
+}
+
+int read(void){
+    FILE *readFile;
+    readFile = fopen("pixels.txt", "r");
+
+    if (readFile == NULL){
+        printf("File cannot be opened or does not exist.\n");
+        return 1;
+    }
+
+    int count = 0;
+    char c;
+    while((c = fgetc(readFile)) != EOF){
+        pixels[count++] = c;
+    }
+
+    pixels[count] = '\0';
+
+    if(readFile) fclose(readFile);
+    return 0;
+}
+
+int write(void){
+    FILE *writeFile;
+    writeFile = fopen("compressed.txt", "w");
+
+    if (writeFile == NULL){
+        printf("File cannot be opened or does not exist.\n");
+        return 1;
+    }
+
+    char c;
+    int i = 0;
+    while((c = compressed[i++]) != '\0'){
+        fputc(c, writeFile);
+    }
+
+    if(writeFile) fclose(writeFile);
+    return 0;
+}
+
+void encode(void){
+    
+}
